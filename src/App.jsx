87fe32863ab96825b89da746c4f287956b9c729e
kadri <kadri.ozcan@outlook.com>
2024-04-22 02:20:17 +0300

    edit room component

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,25 @@ import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
 
-import AddRoom from "./components/room/AddRoom";
 import ExistingRooms from "./components/room/ExistingRooms";
+import Home from "./components/home/Home";
+import EditRoom from "./components/room/EditRoom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <AddRoom />
-      <ExistingRooms />
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRooms />} />
+          </Routes>
+        </Router>
+      </main>
     </>
   );
 }
--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -49,3 +49,23 @@ export async function deleteRoom(roomId) {
     }
 }
 
+// Updates the selected room by id
+export async function updateRoom(roomId, roomData) {
+    const formData = new FormData()
+    formData.append("roomType", roomData.roomType)
+    formData.append("roomPrice", roomData.roomPrice)
+    formData.append("photo", roomData.photo)
+    const response = await api.put(`/rooms/update/${roomId}`)
+    return response
+}
+
+// Get room by its id
+export async function getRoomById(roomId) {
+    try {
+        const response = await api.get(`/rooms/${roomId}`)
+        return response.data
+    } catch (e) {
+        throw new Error(`Error Fetching Room: ${error.message}`)
+    }
+}
+
