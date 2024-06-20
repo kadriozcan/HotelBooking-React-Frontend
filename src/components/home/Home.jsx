cf5d2a63b95a5b247019207eb94be213fcf0f5c0
kadri <kadri.ozcan@outlook.com>
2024-06-20 15:17:48 +0300

    homepage carousel

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import MainHeader from "../layout/MainHeader";
 import HotelService from "../common/HotelService";
 import Parallax from "../common/Parallax";
+import RoomCarousel from "../common/RoomCarousel";
 
 const Home = () => {
   return (
     <section>
       <MainHeader />
       <section className="container">
+        <RoomCarousel />
         <Parallax />
         <HotelService />
         <Parallax />
